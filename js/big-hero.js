80a69542f15d07a0b9c024ace74533ac28d38718
ConorHawk <conornhawkins@gmail.com>
2016-04-29 12:04:26 +1200

    added character screen effects

--- js/big-hero.js
+++ js/big-hero.js
@@ -1,3 +1,22 @@
 $( document ).ready(function(){
+		$(".char").click(function(){
 
+			var clickedId= $(this).attr("id");
+ 
+		if ($(this).siblings(".char-active").length) {
+			$(this).siblings(".char-active").removeClass("char-active");
+			$(this).addClass("char-active");
+		} else {
+			$(this).addClass("char-active");
+		}
+
+
+		if ($(".char-info").siblings(".info-active").length) {
+			$(".char-info").siblings(".info-active").removeClass("info-active");
+			$("."+clickedId+"-info").addClass("info-active");
+		} else {
+			$(".char-info").addClass("info-active");
+		}
+
+	});
 });
